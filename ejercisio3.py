0a3aff1c694e68cc2267d9702b563d2baedcfe42
Hndz93 <88945620+Hndz93@users.noreply.github.com>
2021-09-11 12:18:21 -0600

    Create ejercisio3.py

---
+++ ejercisio3.py
@@ -0,0 +1,11 @@
+ InicioSesion = ("Nombre y clave"):
+print ("Ingrese Usuario : ")
+usuario = input("Parcial")
+print ("Ingrese Clave : ")
+clave = int(input(1234))
+
+if (usuario=="Parcial" and clave==1234):
+
+ print("nombre usuario incorrecto")
+else:
+ print("Clave de acceso incorecto")
