0771ae8eb0019a3acbaef135bd0798e6bfd29b8d
Hndz93 <88945620+Hndz93@users.noreply.github.com>
2021-09-11 11:16:13 -0600

    Update EjercisiosParcial.py

---
+++ EjercisiosParcial.py
@@ -0,0 +1,22 @@
+x=1 
+Compra=float (input) ("ingrese el valor de compra ")
+print ("seleccione dia descuento martes o jueves")
+capturardatos = str(input("descuento del dia ")
+
+if capturardatos == "Martes" :
+    Descuento = compra *0.15
+    Compra = compra - descuento
+
+    print ("se aplico descuento")
+
+elif ==capturados == "Jueves" :
+    descuento = compra *0.15
+    compra = compra - descuento 
+
+print ("se aplico descuento", compra)
+
+
+Else : print ("no selecionastes martes o jueves")
+
+print ("se efectuo pago")
+
