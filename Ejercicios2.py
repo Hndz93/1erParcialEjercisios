5683a92e2a228db5e199195647ff61ae8b98cf73
Hndz93 <88945620+Hndz93@users.noreply.github.com>
2021-09-11 14:05:04 -0600

    Update Ejercicios2.py

---
+++ Ejercicios2.py
@@ -0,0 +1,15 @@
+SueldoSemanal=int(input("ingresa horas trabajadas"))
+if("HorasTrabajadas <= 40 horas") :
+
+ SueldoSemanal= ("Salariohoras <=40horas")
+ monto = "$20 * 40 horas"
+ print ("salario neto de $800")
+
+elif  "Aumento = (SueldoSemanal)*0.05" :
+  monto= "SueldoSemanal+HoraExtra" 
+  
+else : 
+
+  print ( "El sueldo percibido es " +
+str(800) + "  y su aumento fue " + str(5))
+print ("efectuar pago")
